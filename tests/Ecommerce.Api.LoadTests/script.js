27fe47e6368bab04cb8c7ece69987ee572bd0dd8
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-12-18 11:04:16 -0300

    chore: setup k6 load test with report in grafana

--- tests/Ecommerce.Api.LoadTests/script.js
+++ tests/Ecommerce.Api.LoadTests/script.js
@@ -1,19 +1,20 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const VUS = parseInt(__ENV.K6_VUS || '10', 10);
-const DURATION = __ENV.K6_DURATION || '30s';
+const default_vus = 5;
+const target_vus_env = `${__ENV.TARGET_VUS}`;
+const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
 
 export const options = {
-    vus: VUS,
-    duration: DURATION,
-    thresholds: {
-        http_req_duration: ['p(90)<250', 'p(95)<500', 'p(99.9)<1000', 'max<2000', 'avg<500', 'med<500'],
-    },
+  stages: [
+      { duration: "5s", target: target_vus },
+      { duration: "10s", target: target_vus },
+      { duration: "5s", target: 0 }
+  ]
 };
 
 export default function () {
-  const res = http.get('http://ecommerce-nginx:3000/products?page=1&size=1');
+  const res = http.get('http://ecommerce-nginx:3333/products?page=1&size=1000');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
